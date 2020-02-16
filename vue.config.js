e53e5b95801cba88731bb9f469308d9d29e6c482
Andrew Graham <andrew.grahamg@gmail.com>
2020-02-16 12:42:05 -0500

    added publicPath for deploying to github pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/VWBuybackCalculator/'
+  : '/',
 
   pwa: {
     name: 'VW Buyback Calculator'
